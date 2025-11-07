793fb4b85c4751824cf4fbd7fcddbf8d24e7a97f
KSnie <u6510348@au.edu>
2025-11-07 14:02:41 +0800

    FirstCommit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+arr1 = np.arange(1,11)
+print(f"arr1: {arr1}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
